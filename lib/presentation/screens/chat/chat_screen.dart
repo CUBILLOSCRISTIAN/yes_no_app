806de6b5a60e55c1f7b79a451e286f54ccf4fa0b
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-02-28 12:40:29 -0800

    Refactor chat provider and message field box

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -56,7 +56,9 @@ class _ChatView extends StatelessWidget {
             ),
 
             //Caja de texto
-            const MessageFieldBox(),
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
           ],
         ),
       ),
