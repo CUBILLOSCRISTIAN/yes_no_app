d0f79f232f1dde826bf9c38a2a4ae208753b1ded
CUBILLOSCRISTIAN <cccubillos@uninorte.edu.co>
2024-02-29 13:26:27 -0800

    Añadir mensaje y URL de imagen a HerMessaggBubble

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessaggBubble extends StatelessWidget {
-  const HerMessaggBubble({super.key});
-
+  HerMessaggBubble({super.key, required this.message});
+  Message message;
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
@@ -13,18 +16,20 @@ class HerMessaggBubble extends StatelessWidget {
         Container(
           decoration: BoxDecoration(
               color: colors.secondary, borderRadius: BorderRadius.circular(20)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
             child: Text(
-              'Hola mundo',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+              message.text,
+              style: const TextStyle(color: Colors.white, fontSize: 20),
             ),
           ),
         ),
         const SizedBox(
           height: 5,
         ),
-        _ImageBubble(),
+        _ImageBubble(
+          imageUrl: message.imageUrl,
+        ),
         const SizedBox(
           height: 10,
         )
@@ -34,6 +39,8 @@ class HerMessaggBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+  const _ImageBubble({super.key, this.imageUrl});
+  final String? imageUrl;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -41,7 +48,7 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        'https://yesno.wtf/assets/yes/9-6403270cf95723ae4664274db51f1fd4.gif',
+        imageUrl!,
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
